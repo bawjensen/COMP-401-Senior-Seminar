63cbae660440a97717098168160d137744c72089
Bryan Jensen <bryanawjensen@gmail.com>
2015-02-17 13:37:54 -0500

    Commenting html and js, and adding png of AI fsm

--- project1/script.js
+++ project1/script.js
@@ -16,25 +16,19 @@ function initializePortfolio() {
 
 // ==================================== Main page scripts ============================
 
-/*
-
-Blog layers
-
-Blog
-\_ Topic (i.e. Ramblings, Tech, Snippets)
-    \_ Post ('What I love about Programming', etc.)
-
-*/
-
+// Separated function to scroll smoothly to the blog div from anywhere on page
 function scrollToBlog() {
     $('html, body').animate({
         scrollTop: $('#blog-container').offset().top
     }, 250);
 }
 
+// Global blog variable
 var blog = {};
 
+// Initialization call to set everything up
 function initializeMainPage() {
+    // When any state transition is clicked, call the corresponding function
     $(document).on('click', '.state-transition', function(evt) {
         var transitionId = $(this).data('state');
 
@@ -43,112 +37,157 @@ function initializeMainPage() {
         }
     });
 
+    // Set up database connection
     var topics = new Firebase("https://personal-blog.firebaseio.com/");
 
+    // When a value is reported by the database
     topics.on("value", function(data) {
         var topicsObj = data.val() ? data.val().topics : {};
 
+        // Load template
         var topicLinksTemplate = blogTemplate({ topicsObj: topicsObj });
 
+        // Set up and fill container for the HTMLs of the fleshed-out templates
         var topicHtmls = {};
         for (var key in topicsObj) {
             topicHtmls[key] = topicTemplate({ topic: topicsObj[key] });
         }
 
-        blog.layers = {
+        // All potential states and their transitions
+        blog.states = {
+            // Page is loaded, but no action has been taken with respect to the blog
             'first': {
                 transitions: {
+                    // The introduction to the blog should happen
                     'intro': function(evt) {
+                        // Fetch the HTML of the given topic and create an element out of it
                         var $newDiv = $($.parseHTML(topicLinksTemplate));
 
                         $newDiv.appendTo('#content');
 
                         scrollToBlog();
 
-                        blog.state = blog.layers.second;
+                        // Set the new state
+                        blog.state = blog.states.second;
                     }
                 }
             },
+            // The blog has been 'opened' and the user is looking at the topics of the blog
             'second': {
                 transitions: {
+                     // The 'home' option has been selected, meaning the blog should close
                     'home': function(evt) {
                         $('#blog-container').remove();
-                        blog.state = blog.layers.first;
+
+                        // Set the new state
+                        blog.state = blog.states.first;
                     },
+
+                    // The introduction to the blog should happen
                     'intro': scrollToBlog,
+
+                    // A topic has been selected, and should open up with its posts showing
                     'topic': function(evt) {
+                        // Fetch the HTML of the given topic and create an element out of it
                         var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
                         var topics = $('#topics-container');
 
-                        // Manually set the margin, make it floated, and display block (removes funkiness with animation)
+                        // Manually set attributes to remove buginess with animation
                         topics.css('margin-left', topics.css('margin-left'));
                         topics.css('float', 'left');
                         topics.css('display', 'block');
 
+                        // Animate the opening of the posts
                         topics.animate({
                             marginLeft: 40,
                             marginRight: 40,
-                            // fontSize: 30,
-                            // borderRightColor: 'white',
-                            // borderRightWidth: 1,
                             width: 200
                         }, 250, function after() {
-                            // topics.css('line-height', topics.css('line-height'));
-                            // topics.css('font-size', '20px');
                             $newDiv.appendTo($('#blog-container'));
                         });
 
-                        blog.state = blog.layers.third;
+                        // Set the new state
+                        blog.state = blog.states.third;
                     }
                 }
             },
+            // A topic for the blog has been selected, and the user is viewing the posts in that topic
             'third': {
                 transitions: {
+                     // The 'home' option has been selected, meaning the blog should close
                     'home': function(evt) {
                         $('#blog-container').remove();
-                        blog.state = blog.layers.first;
+
+                        // Set the new state
+                        blog.state = blog.states.first;
                     },
+
+                    // The introduction to the blog should happen
                     'intro': scrollToBlog,
+
+                    // A topic has been selected, and should open up with its posts showing
                     'topic': function(evt) {
+                        // Fetch the HTML of the given topic and create an element out of it
                         var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
                         $('#posts').replaceWith($newDiv);
                     },
+
+                    // A specific post within a topic has been selected, and should open with its full text
                     'post': function(evt) {
                         var $clicked = $(evt.target);
 
                         $clicked.closest('.post').find('.preview, .text').toggle();
 
-                        blog.state = blog.layers.fourth;
+                        // Set which post is currently showing
                         blog.state.$showing = $clicked;
+
+                        // Set the new state
+                        blog.state = blog.states.fourth;
                     },
+
+                    // The close option has been selected for a topic
                     'close': function(evt) {
                         $('#posts').remove();
                         $('#topics-container').attr('style', '');
-                        blog.state = blog.layers.second;
+
+                        // Set the new state
+                        blog.state = blog.states.second;
                     }
                 }
             },
+            // The user is looking at the full text of a specific blog post
             'fourth': {
                 transitions: {
+                     // The 'home' option has been selected, meaning the blog should close
                     'home': function(evt) {
                         $('#blog-container').remove();
-                        blog.state = blog.layers.first;
+
+                        // Set the new state
+                        blog.state = blog.states.first;
                     },
+
+                    // The introduction to the blog should happen
                     'intro': scrollToBlog,
+
+                    // A topic has been selected, and should open up with its posts showing in place of the old topic's
                     'topic': function(evt) {
+                        // Fetch the HTML of the given topic and create an element out of it
                         var $newDiv = $($.parseHTML(topicHtmls[evt.target.id]));
 
                         $('#posts').replaceWith($newDiv);
                     },
+
+                    // An alternate post has been selected while a certain one is already open
                     'post': function(evt) {
                         var $clicked = $(evt.target).closest('.post');
 
                         $clicked.find('.preview, .text').toggle();
 
                         if (blog.state.$showing && ($clicked[0] == blog.state.$showing[0])) {
-                            blog.state = blog.layers.third;
+                            // No posts are open, so go back to the third state
+                            blog.state = blog.states.third;
                         }
                         else if (blog.state.$showing) {
                             blog.state.$showing.find('.preview, .text').toggle();
@@ -158,29 +197,29 @@ function initializeMainPage() {
                             blog.state.$showing = $clicked;
                         }
                     },
+
+                    // The close option has been selected for a topic, going back to state 2
                     'close': function(evt) {
                         $('#posts').remove();
                         $('#topics-container').attr('style', '');
-                        blog.state = blog.layers.second;
+
+                        // Set the new state
+                        blog.state = blog.states.second;
                     }
                 },
+                // Initially no posts are showing
                 $showing: null
             }
         };
 
-        // for 
-
-        blog.state = blog.layers.first;
-
-        // for (var topic in topicsObj) {
-        //     blog.state.transitions[topic] = function() {
-        //     };
-        // }
+        // Initialize the blog as being in the first state
+        blog.state = blog.states.first;
     });
 }
 
 // ==================================== OnLoad scripts ===============================
 
+// Only initialize scripts for the page that is currently active
 $(function() {
     if (CURRENT_ROUTE.match('portfolio')) {
         initializePortfolio();
